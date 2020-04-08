a93d7632c7c6fa58fae17721b6c637160ef4a62e
MultiPain <32590972+MultiPain@users.noreply.github.com>
2020-04-08 17:08:46 +0900

    Update vShader.glsl

--- 2D_SmoothLight/assets/vShader.glsl
+++ 2D_SmoothLight/assets/vShader.glsl
@@ -1,10 +1,11 @@
 attribute highp vec3 vVertex;
 attribute mediump vec2 vTexCoord;
 uniform highp mat4 vMatrix;
+uniform mediump vec4 vTexInfo;
 varying mediump vec2 fTexCoord;
 
 void main() {
   vec4 vertex = vec4(vVertex,1.0);
   gl_Position = vMatrix*vertex;
-  fTexCoord=vTexCoord;
+  fTexCoord=vTexCoord/vTexInfo.xy;
 }
